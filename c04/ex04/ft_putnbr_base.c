4a9e1be2311cb97709112136cc3e474c3ec1d7ce
emind01 <emin.drn@icloud.com>
2024-08-24 21:01:49 +0300

    Add files via upload

---
+++ c04/ex04/ft_putnbr_base.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muduran <muduran@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 20:16:25 by muduran           #+#    #+#             */
+/*   Updated: 2024/08/24 20:39:17 by muduran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (base[i])
+	{
+		j = i + 1;
+		if (base[i] == '-' || base[i] == '+')
+			return (0);
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (i < 2)
+		return (0);
+	return (i);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	unsigned int	size;
+	unsigned int	num;
+
+	size = check_base(base);
+	if (size == 0)
+		return ;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		num = -nbr;
+	}
+	else
+		num = nbr;
+	if (nbr >= size)
+		ft_putnbr_base(num / size, base);
+	ft_putchar(base[num % size]);
+}
